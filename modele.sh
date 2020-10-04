08e4c37cefc8a8705932ec943171d0ffc04a3b22
Jonas Karg <jonas.karg@hotmail.com>
2020-10-04 21:52:16 +0200

    Added function to reduce code redundancy.

--- modele.sh
+++ modele.sh
@@ -2,6 +2,39 @@
 
 TEMPLATES_DIR=$(dirname "$0")/templates
 
+# Check if the name parameter is provided.
+function check_parameter() {
+  if [ -z "$1" ]; then
+    echo "You need to specify a name."
+    exit 1
+  fi
+}
+
+# Check if template exists.
+function check_template() {
+  if [ ! -d $TEMPLATES_DIR/$1 ]; then
+    echo "No such template."
+    exit 1
+  fi
+}
+
+# Create template from current folder.
+function create_template() {
+  mkdir $TEMPLATES_DIR/$1
+  cp $PWD/* $TEMPLATES_DIR/$1
+  echo "Successfully $2 template \"$1\" from the current folder."
+}
+
+# Wait for user confirmation.
+function confirm() {
+  read -p "Continue? [y/N] "
+
+  if [[ ! $REPLY =~ [yY] ]]; then
+    echo "Aborting."
+    exit 1
+  fi
+}
+
 if [ -z "$1" ]; then
   echo "No template specified."
   exit 1
@@ -9,11 +42,7 @@ fi
 
 # Create new template
 if [ $1 == "new" ]; then
-  # Check if the name parameter is provided.
-  if [ -z "$2" ]; then
-    echo "You need to specify a name."
-    exit 1
-  fi
+  check_parameter $2
 
   # Check if template already exists.
   if [ -d $TEMPLATES_DIR/$2 ]; then
@@ -22,50 +51,40 @@ if [ $1 == "new" ]; then
   fi
 
   # Create new template from current foder.
-  mkdir $TEMPLATES_DIR/$2
-  cp $PWD/* $TEMPLATES_DIR/$2
-  echo "Successfully created template \"$2\" from the current folder."
+  create_template $2 created
   exit 0
 fi
 
 # Update template
-if [ $1 == "new" ]; then
-  # Check if the name parameter is provided.
-  if [ -z "$2" ]; then
-    echo "You need to specify a name."
-    exit 1
+if [ $1 == "update" ]; then
+  check_parameter $2
+  check_template $2
+
+  # Ask for confirmation.
+  if [ ! -z "$(ls -A $PWD)" ]; then
+    echo "You are about to overwrite a template."
+    confirm
   fi
 
-  # Check if template exists.
-  if [ ! -d $TEMPLATES_DIR/$2 ]; then
-    echo "No such template."
-    exit 1
-  fi
-
-  # Create new template from current foder.
   rm -rf $TEMPLATES_DIR/$2
-  mkdir $TEMPLATES_DIR/$2
-  cp $PWD/* $TEMPLATES_DIR/$2
-  echo "Successfully updated template \"$2\" from the current folder."
+  create_template $2 updated
   exit 0
 fi
 
-# Check if template exists.
-if [ ! -d "$TEMPLATES_DIR/$1" ]; then
-  echo "No such template."
+check_template $1
+
+if [ ! -z $2 ]; then
+  echo "Too many parameters."
   exit 1
 fi
 
 # Ask for confirmation if folder not empty.
 if [ ! -z "$(ls -A $PWD)" ]; then
-  read -p "This folder is not empty. Continue? [Y/n] "
-
-  if [ ! $REPLY == ^[Yy]$ ]; then
-    echo "Abort. No changes were made."
-    exit 1
-  fi
+  echo "This folder is not empty."
+  confirm
 fi
 
+# Create project from template
 cp -r $TEMPLATES_DIR/$1/* $PWD
 echo "Successfully created project from template."
 exit 0
